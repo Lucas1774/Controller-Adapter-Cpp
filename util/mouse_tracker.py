0c5c9a9d7fdda4d452a04e47981a8bf163856d5a
Lucas1774 <lucas.115@hotmail.com>
2024-08-30 01:14:19 +0200

    Enhance mouse position tracker script

--- util/mouse_tracker.py
+++ util/mouse_tracker.py
@@ -1,9 +1,28 @@
 from pynput.mouse import Controller
 import time
+import keyboard
 
 mouse = Controller()
+positions = []
+row = []
 
 while True:
     position = mouse.position
-    print(f"{position[0]}, {position[1]}")
-    time.sleep(1)
+    if keyboard.is_pressed('r'):
+        row.append(position)
+        print(f"Saved position: {position}")
+    if keyboard.is_pressed('s'):
+        positions.append(row)
+        print(f"Saved row: {row}")
+        row = []
+    if keyboard.is_pressed('q'):
+        break
+    time.sleep(0.5)
+with open('positions.txt', 'w') as f:
+    f.write("{ \n")
+    for row in positions:
+        f.write("{")
+        for position in row:
+            f.write(f"{{{str(position[0])}, {str(position[1])}}}, ")
+        f.write("}, \n")
+    f.write("};")
