287c867c2dcbd25003657bc0e2ee8db5217f5a73
Lucas1774 <lucas.115@hotmail.com>
2024-09-01 17:43:33 +0200

    Improve mouse position tracker script

--- util/mouse_tracker.py
+++ util/mouse_tracker.py
@@ -1,28 +1,38 @@
-from pynput.mouse import Controller
-import time
+from pynput.mouse import Listener as MouseListener, Button
 import keyboard
 
-mouse = Controller()
 positions = []
 row = []
 
-while True:
-    position = mouse.position
-    if keyboard.is_pressed('r'):
-        row.append(position)
-        print(f"Saved position: {position}")
-    if keyboard.is_pressed('s'):
-        positions.append(row)
-        print(f"Saved row: {row}")
-        row = []
-    if keyboard.is_pressed('q'):
-        break
-    time.sleep(0.5)
-with open('positions.txt', 'w') as f:
-    f.write("{ \n")
-    for row in positions:
-        f.write("{")
-        for position in row:
-            f.write(f"{{{str(position[0])}, {str(position[1])}}}, ")
-        f.write("}, \n")
-    f.write("};")
+
+def on_click(x, y, button, pressed):
+    """
+    - ctrl + left click to save a position
+    - ctrl + middle click to save a row
+    - ctrl + right click to dump to file and exit
+    """
+    global row
+    global positions
+    if pressed and keyboard.is_pressed("ctrl"):
+        if button == Button.left:
+            row.append((x, y))
+            print(f"Saved pos: {(x, y)}")
+        elif button == Button.middle:
+            positions.append(row)
+            print(f"Saved row: {row}")
+            row = []
+        elif button == Button.right:
+            with open("positions.txt", "w") as f:
+                f.write("{ \n")
+                for row in positions:
+                    f.write("{")
+                    for position in row:
+                        f.write(f"{{{position[0]}, {position[1]}}}, ")
+                    f.write("}, \n")
+                f.write("};")
+            print("Saved file: positions.txt")
+            return False
+
+
+with MouseListener(on_click=on_click) as mouse_listener:
+    mouse_listener.join()
